88856618651959daedcf84aadf8c19415858b8ab
Thiago-Gomes-Araujo <thiagogomes543@hotmail.com>
2024-05-30 01:58:31 -0300

    Usando before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -21,12 +20,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id]) 
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -35,7 +31,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-     @article = Article.find(params[:id])
      @article.destroy
      redirect_to root_path
   end
@@ -44,4 +39,9 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
 end
